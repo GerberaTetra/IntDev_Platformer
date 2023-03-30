9923bd6e03b2dd8f43e75ad4e3e989ff3129ca66
DOOM-SLAYER\thene <dhv224@nyu.edu>
2023-03-30 15:14:24 -0400

    2nd commit

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor.Animations;
 
 public class PlayerControl : MonoBehaviour
 {
+    public SpriteRenderer spi;
+    public Animator animator;
+    public RuntimeAnimatorController newController;
 
     float horizontalMove;
     public float speed;
@@ -16,8 +20,18 @@ public class PlayerControl : MonoBehaviour
     bool dead = false;
     bool win = false;
     bool win2 = false;
+    bool win3 = false;
+    bool moveRight = false;
+    bool moveLeft = false;
+    bool moveRightU = false;
+    bool moveLeftU = false;
 
+    public GameObject id1;
+    public GameObject id2;
+    public GameObject id3;
+    public GameObject id4;
 
+    public int id = 0;
     public float castDist = 0.2f;
     public float gravityScale = 5f;
     public float gravityFall = 40f;
@@ -25,15 +39,21 @@ public class PlayerControl : MonoBehaviour
 
     bool jump = false;
 
+
     // Start is called before the first frame update
     void Start()
     {
         myBody = GetComponent<Rigidbody2D>();
+        spi = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+
+
         horizontalMove = Input.GetAxis("Horizontal");
 
         if (Input.GetButtonDown("Jump") && grounded)
@@ -48,42 +68,86 @@ public class PlayerControl : MonoBehaviour
             gravityScale = 3;
             gravityFall = 4;
             flip = false;
-        }else if(Input.GetButtonDown("Fire1") && flip == false)
+            spi.flipY = false;
+
+        }
+        else if (Input.GetButtonDown("Fire1") && flip == false)
         {
             gravityScale = -3;
             gravityFall = -4;
             flip = true;
+            spi.flipY = true;
         }
         //scene changes for win/loss
-        
+
     }
     void FixedUpdate()
     {
         float moveSpeed = horizontalMove * speed;
 
+        //going right
+        /*   if (moveSpeed > 0)
+           {
+               moveRight = true;
+           }
+
+           if (moveRight == true)
+           {
+               animator.SetBool("run", true);
+               animator.SetBool("idle", false);
+               spi.flipX = false;
+           }*/
+        //going left
         if (jump)
         {
             myBody.AddForce(Vector2.up * jumpLimit, ForceMode2D.Impulse);
             jump = false;
-            
+            animator.SetBool("jump", true);
+
         }
+        else if (horizontalMove > 0)
+        {
+            animator.SetBool("run", true);
+            animator.SetBool("idle", false);
+            spi.flipX = false;
+        }
+        else if (horizontalMove < 0)
+        {
+            animator.SetBool("run", true);
+            animator.SetBool("idle", false);
+            spi.flipX = true;
+        }
+        else
+        {
+            animator.SetBool("run", false);
+            animator.SetBool("idle", true);
+        }
+
+
+
+        
         if (myBody.velocity.y >= 0)
         {
             myBody.gravityScale = gravityScale;
-        }else if (myBody.velocity.y < 0)
+        }
+        else if (myBody.velocity.y < 0)
         {
             myBody.gravityScale = gravityFall;
         }
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, castDist);
-        
+
         if (dead == true)
         {
             SceneManager.LoadScene("Game Over");
         }
         if (win == true)
         {
-            SceneManager.LoadScene("Level2");
+            SceneManager.LoadScene("Level3");
+        if (win3 == true)
+            {
+                SceneManager.LoadScene("Level2");
+            }
         }
         if (win2 == true)
         {
@@ -92,23 +156,29 @@ public class PlayerControl : MonoBehaviour
 
         Debug.DrawRay(transform.position, Vector2.down * castDist, Color.red);
 
-        if (hit.collider != null && hit.transform.name == "Ground")
+        if (hit.collider != null && hit.transform.tag == "ground")
         {
             grounded = true;
+            animator.SetBool("jump", false);
         }
         else
         {
             grounded = false;
         }
-        if (hit.collider != null && hit.transform.name == "Spike")
+
+        if (hit.collider != null && hit.transform.tag == "spike")
         {
             dead = true;
         }
-        if (hit.collider != null && hit.transform.name == "Flag")
+        if ((hit.collider != null && hit.transform.name == "Flag") && (Input.GetButtonDown("Up")))
         {
             win = true;
         }
-        if (hit.collider != null && hit.transform.name == "FlagWIN")
+        if ((hit.collider != null && hit.transform.name == "Flag2") && (Input.GetButtonDown("Up")))
+        {
+            win3 = true;
+        }
+        if ((hit.collider != null && hit.transform.tag == "FlagWIN") && (Input.GetButtonDown("Up")))
         {
             win2 = true;
         }
@@ -117,23 +187,24 @@ public class PlayerControl : MonoBehaviour
             dead = true;
         }
 
+        if ((hit.collider != null && hit.transform.tag == "id1"))
+        {
+            id += 1;
+            Destroy(id1);
+        }
+        if ((hit.collider != null && hit.transform.name == "id2"))
+        {
+            win = true;
+        }
+        if ((hit.collider != null && hit.transform.name == "id3"))
+        {
+            win = true;
+        }
+        if ((hit.collider != null && hit.transform.name == "id4"))
+        {
+            win = true;
+        }
 
         myBody.velocity = new Vector3(moveSpeed, myBody.velocity.y, 0);
     }
-    /*
-    public void LoadGame()
-    {
-        if (dead == true)
-        {
-            SceneManager.LoadScene("Game Over");
-        }
-        if (win == true)
-        {
-            SceneManager.LoadScene("Win1");
-        }
-        if (win2 == true)
-        {
-            SceneManager.LoadScene("Win2");
-        }
-    }*/
 }
